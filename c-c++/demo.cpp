c990629c78b8df38c1753c36425127f0be26b16d
md-pranta <juhurenurulhaque@gmail.com>
2023-04-01 05:48:19 +0600

    setup

---
+++ c-c++/demo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// Main() function: where the execution of program begins
+int main()
+{
+	// prints hello world
+	cout << "Hello World\n";
+
+	return 0;
+}
